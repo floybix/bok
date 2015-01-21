ec8cf407a3dcef7e9be0f5e252630610aa3dbf20
Felix Andrews <felix@nfrac.org>
2015-01-21 13:33:03 +0800

    altitude game

--- runner/src/org/nfrac/hatto/games.clj
+++ runner/src/org/nfrac/hatto/games.clj
@@ -2,7 +2,7 @@
   (:require [org.nfrac.hatto.entities :as ent :refer [with-pois map->Entity]]
             [org.nfrac.hatto.creatures :as creatures]
             [org.nfrac.cljbox2d.core :refer :all]
-            [org.nfrac.cljbox2d.vec2d :refer [v-add polar-xy]]))
+            [org.nfrac.cljbox2d.vec2d :refer [v-add polar-xy v-interp]]))
 
 ;; =============================================================================
 
@@ -306,61 +306,96 @@
 (defmethod build* :altitude
   [type players _]
   (let [world (new-world)
-        surface (for [x (range -20 20.01 0.2)]
-                  [x (+ (/ (Math/pow x 4)
-                           (Math/pow 20 3))
-                        (* (Math/cos (* x 4))
-                           (/ (* x x) (* 20 20))
-                           1.5))])
-        pois (for [z [0 1/3 2/3 1]]
-               (nth surface (* z (dec (count surface)))))
-        ground (apply body! world {:type :static}
-                      (edge-chain surface
-                                  {:friction 1}))
-        plat-fx {:shape (edge [-5 0] [5 0])
+        surface [[-16 0] [-4 0] [0 -2] [4 0] [16 0]]
+        ground (with-pois
+                 (apply body! world {:type :static}
+                        (edge-chain surface
+                                    {:friction 1}))
+                 surface)
+        ceil-y 16
+        ceiling (body! world {:type :static}
+                       {:shape (edge [-16 ceil-y]
+                                     [16 ceil-y])})
+        stal-yend 10
+        stal-xoff 1
+        stal-pois (for [sign [-1 1]
+                        frac (range 0.1 0.9 0.2)]
+                    (v-interp [0 stal-yend]
+                              [(* sign stal-xoff) ceil-y]
+                              frac))
+        stalactite (with-pois
+                     (apply body! world {:type :static}
+                            {:shape (polygon [[stal-xoff ceil-y]
+                                              [(- stal-xoff) ceil-y]
+                                              [0 stal-yend]])
+                             :friction 1}
+                            (for [pos stal-pois
+                                  :let [sign (if (neg? (first pos)) -1 1)]]
+                              {:shape (edge pos (v-add pos [(* sign 0.5) 0.1]))
+                               :friction 1}))
+                     stal-pois)
+        rope-length 10
+        swing-pois [[-2 0] [2 0]]
+        swing (with-pois
+                (body! world {:position [0 (- ceil-y rope-length)]}
+                       {:shape (box 2 0.1)
+                        :friction 1})
+                swing-pois)
+        ropes (doall
+               (for [[x y] swing-pois]
+                 (joint! {:type :rope
+                          :body-a ceiling
+                          :body-b swing
+                          :anchor-a [(* x 2) ceil-y]
+                          :anchor-b [x y]
+                          :max-length rope-length})))
+        block-fx {:shape (box 2 1.5)
+                  :density 2
+                  :friction 1}
+        block-pois [[-2 1.5]
+                    [2 1.5]]
+        blocks (for [[sign s] [[-1 "left"] [1 "right"]]]
+                 [(keyword (str "block-" s))
+                  (with-pois
+                    (body! world {:position [(* sign 13) 1.5]}
+                           block-fx)
+                    block-pois)])
+        mini-fx {:shape (box 0.5 0.5)
+                 :density 2
                  :friction 1}
-        plat-pois [[-5 0] [5 0]]
-        plat-right (body! world {:type :static
-                                 :position [8 15]}
+        mini-pois [[-1 1]
+                   [1 1]
+                   [1 -1]
+                   [-1 -1]]
+        minis (for [[sign s] [[-1 "left"] [1 "right"]]]
+                [(keyword (str "mini-" s))
+                 (with-pois
+                   (body! world {:position [(* sign 4.5) 0.5]}
+                          mini-fx)
+                   mini-pois)])
+        plat-fx {:shape (edge [-2 0] [2 0])
+                 :friction 1}
+        plat-pois [[-2 0] [2 0]]
+        plats (for [[sign s] [[-1 "left"] [1 "right"]]]
+                [(keyword (str "plat-" s))
+                 (with-pois
+                   (body! world {:type :static
+                                 :position [(* sign 8) 6]}
                           plat-fx)
-        plat-left (body! world {:type :static
-                                :position [-8 15]}
-                         plat-fx)
+                   plat-pois)])
         arena (map->Entity
                {:entity-type :arena
                 :arena-type type
-                :components {:ground (with-pois ground pois)
-                             :plat-left (with-pois plat-left plat-pois)
-                             :plat-right (with-pois plat-right plat-pois)}})
-        ;; place food by scanning down from above at regular intervals
-        food-pos (for [sign [-1 1]
-                       xa (concat [2 4 6]
-                                  (range 10.5 20))
-                       :let [x (* sign xa)]]
-                   (let [[rc] (raycast world [x 20] [x -10] :closest)]
-                     (if rc
-                       (v-add (:point rc) [0 0.2])
-                       (do (println (str "raycast missed: " x))
-                           [0 0]))))
-        food-fx {:shape (polygon [[0 0.5] [-0.25 0] [0.25 0]])
-                 :density 5 :restitution 0 :friction 1}
-        food (map->Entity
-              {:entity-type :food
-               :components (into {}
-                                 (map-indexed (fn [i pos]
-                                                (let [k (keyword (str "food-" i))]
-                                                  [k(with-pois
-                                                      (body! world {:position pos}
-                                                             food-fx)
-                                                      [[0 0]])]))
-                                              food-pos))})
-        starting-pts (map vector [-10 10 0 -5 5] (repeat 10))]
+                :components (into {:ground ground
+                                   :stalactite stalactite}
+                                  (concat blocks minis plats))})
+        starting-pts (map vector [-10 10] (repeat 2.5))]
     (->
      (assoc empty-game
        :world world
        :entities {:arena arena
-                  :food food}
-       :camera {:width 40 :height 20 :x-left -20 :y-bottom -1}
+                  }
+       :camera {:width 40 :height 20 :x-left -20 :y-bottom -3}
        :check-end (fn [game]
                     (check-highest game 60)))
      (add-players players starting-pts))))
