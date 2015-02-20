7aad188ff1d0641d9238e914609d3dc39826eec9
Felix Andrews <felix@nfrac.org>
2015-02-20 16:30:30 +0800

    altitude-playing bipoid: a more encapsulated upward-check

--- players/clojure/src/org/nfrac/bok/examples/bipoid_alti.clj
+++ players/clojure/src/org/nfrac/bok/examples/bipoid_alti.clj
@@ -15,25 +15,37 @@
 (def down-left (* PI -0.8))
 (def down-right (* PI -0.2))
 
-(defn update-grid-cache
-  [grid-cache grid-pos rc]
+(defn update-upward-check
+  "Helps to work out which direction to go in to get higher up.. Map
+   `m` should be this function's result from the previous time step,
+   and `rc` the raycast perception data. Returns keys `:dir` - which
+   direction seems to be upwards: -1 left, 1 right, 0 not yet
+   determined; and `:next-rc` - the angle for the next raycast." [m
+   rc]
   (if-not rc
-    grid-cache
-    (let [this-cache (get grid-cache grid-pos {})
-          a (:angle rc)
+    ;; initialisation
+    (assoc m
+      :next-rc down-left
+      :dir 0)
+    (let [a (:angle rc)
           left (if (angle-left? a)
                  (or (:distance rc) 100)
-                 (:left this-cache))
+                 (:left m))
           right (if (not (angle-left? a))
                   (or (:distance rc) 100)
-                  (:right this-cache))]
-      (assoc grid-cache grid-pos
-             {:left left :right right
-              ;; if left distance is less (i.e. upwards), go there
-              :dir (if (and left right)
-                     (if (< left right) -1 1)
-                     ;; return dir 0 if raycasts not completed yet
-                     0)}))))
+                  (:right m))]
+      (assoc m
+        :left left
+        :right right
+        ;; alternate raycast side
+        :next-rc (if (angle-left? a)
+                   down-right
+                   down-left)
+        ;; if left distance is lower (i.e. left is upwards), go there.
+        :dir (if (and left right)
+               (if (< left right) -1 1)
+               ;; return dir 0 if raycasts not completed yet
+               0)))))
 
 (defn my-action-fn
   [state]
@@ -42,14 +54,10 @@
         me (get entities my-key)
         {:keys [head leg-a1 leg-a2 leg-b1 leg-b2]} (:components me)
         eye (first (:points head))
-        ;; treat positions as comparable if within the integer meter
-        grid-pos (mapv int (:position eye))
-        ;; look up and compare distance at down-left vs down-right
-        grid-cache (-> (:grid-cache state {})
-                       (update-grid-cache grid-pos (:raycast (:current state))))
-        dir (get-in grid-cache [grid-pos :dir] 0)
-        last-rc-side (:last-rc-side state :left)
-        this-rc-side (case last-rc-side :left :right, :right :left)
+        ;; look up and compare raycast distances to decide direction
+        upward-check (-> (:upward-check state {})
+                         (update-upward-check (:raycast (:current state))))
+        dir (:dir upward-check)
         BRACE -0.7
         actions
         {:joint-motors
@@ -57,13 +65,10 @@
           :leg-a2 (turn-towards (* dir BRACE) (:angle leg-a2) 0 10)
           :leg-b2 (turn-towards (* dir BRACE) (:angle leg-b2) 0 10)
           }
-         :raycast (case this-rc-side
-                    :left down-left
-                    :right down-right)}]
+         :raycast (:next-rc upward-check)}]
     (assoc state
       :actions actions
-      :last-rc-side this-rc-side
-      :grid-cache grid-cache)))
+      :upward-check upward-check)))
 
 (defn main
   "For REPL use. Pass an `(atom {})` for peeking at the state."
